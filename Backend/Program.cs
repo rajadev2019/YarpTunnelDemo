b6dfba221b501e51ffa5cadbda51c20fe8c72ff6
rajadev2019 <raja.dev2019@gmail.com>
2023-02-17 21:41:32 +0530

    Working solution with two apis

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -5,6 +5,7 @@ builder.Services.AddReverseProxy()
 
 // This is the HTTP/2 endpoint to register this app as part of the cluster endpoint
 var url = builder.Configuration["Tunnel:Url"]!;
+// https API response problem https://github.com/microsoft/reverse-proxy/issues/1800
 
 builder.WebHost.UseTunnelTransport(url);
 
